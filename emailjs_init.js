dba74b04a961105e9731ee2e9da40e1d3e32b4a4
Jean Justen <jean.cjm@hotmail.com>
2024-09-24 15:54:40 -0300

    fix

--- emailjs_init.js
+++ emailjs_init.js
@@ -1,9 +1,2 @@
-const email_js_client_key = process.env.API_KEY;
+// const email_js_client_key = process.env.API_KEY;
 
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-        publicKey: "kGPFPWjqp-T9f7Dml", 
-        privateKey: email_js_client_key,
-    });
-})();
