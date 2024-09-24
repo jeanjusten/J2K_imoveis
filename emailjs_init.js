afcfb30611549ea015f02db490ac04b5778fcb1d
Jean Justen <jean.cjm@hotmail.com>
2024-09-24 14:51:11 -0300

    updated private key

--- emailjs_init.js
+++ emailjs_init.js
@@ -3,6 +3,7 @@ const email_js_client_key = process.env.API_KEY;
 (function() {
     // https://dashboard.emailjs.com/admin/account
     emailjs.init({
-        publicKey: email_js_client_key, 
+        publicKey: "kGPFPWjqp-T9f7Dml", 
+        privateKey: email_js_client_key,
     });
 })();
